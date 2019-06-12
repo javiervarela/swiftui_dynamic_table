fe2dc3cd7f0f8ec91513ff74f2afb607c4ba8957
Javier Angel Varela Cebey <gekito@gmail.com>
2019-06-12 22:09:46 +0200

    Initial Commit

---
+++ Dynamic Table/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Dynamic Table
+//
+//  Created by Javier Angel Varela Cebey on 12/06/2019.
+//  Copyright © 2019 Javier Angel Varela Cebey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
